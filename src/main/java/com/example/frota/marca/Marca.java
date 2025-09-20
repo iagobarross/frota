c56e57f32aad530b10ff5e522fc84eb4de1ed521
iagobarross <iago.demeo@fatec.sp.gov.br>
2025-09-20 11:21:10 -0300

    CRUD Marca

--- src/main/java/com/example/frota/marca/Marca.java
+++ src/main/java/com/example/frota/marca/Marca.java
@@ -1,33 +1,41 @@
 package com.example.frota.marca;
 
-
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-	@Entity
-	@Table(name = "marca")
-	@Getter
-	@Setter
-	@NoArgsConstructor
-	@AllArgsConstructor
-	@EqualsAndHashCode(of ="id")
-	public class Marca {
-		@Id
-		@GeneratedValue(strategy=GenerationType.IDENTITY)
-		private long id;
-		private String nome;
-		private String pais;
-		
-	 
+@Entity
+@Table(name = "marca")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Marca {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String nome;
+	private String pais;
+
+	public Marca(DadosCadastroMarca dados) {
+		this.nome = dados.nome();
+		this.pais = dados.pais();
 	}
 	
-	
+	public void atualizarInformacoes(@Valid DadosAtualizacaoMarca dados) {
+		if (dados.nome() != null) {
+			this.nome = dados.nome();
+			this.pais = dados.pais();
+		}
+	}
+
+}
--- src/main/java/com/example/frota/marca/MarcaController.java
+++ src/main/java/com/example/frota/marca/MarcaController.java
@@ -2,27 +2,59 @@ package com.example.frota.marca;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/marca")
 public class MarcaController {
- 
+
 	@Autowired
 	private MarcaRepository marcaRepository;
- 
- 
-	@GetMapping              
-	public String carregaPaginaListagem(Model model){
+
+	@GetMapping
+	public String carregaPaginaListagem(Model model) {
 		System.out.println("Acessando /marca/listagem");
 		model.addAttribute("lista", marcaRepository.findAll());
-		return "marca/listagem";              
+		return "marca/listagem";
 	}
-	@GetMapping ("/formulario")             
-	public String carregaPaginaFormulario(Model model){
-		return "marca/formulario";              
+
+	@GetMapping("/formulario")
+	public String carregaPaginaFormulario(Long id, Model model) {
+		if (id != null) {
+			var marca = marcaRepository.getReferenceById(id);
+			model.addAttribute("marca", marca);
+		}
+		return "marca/formulario";
 	}
- 
+
+	@PostMapping
+	@Transactional
+	public String cadastrar(@Valid DadosCadastroMarca dados) {
+		marcaRepository.save(new Marca(dados));
+		return "redirect:marca";
+	}
+
+	@DeleteMapping
+	@Transactional
+	public String removeMarca(Long id) {
+		marcaRepository.deleteById(id);
+		return "redirect:marca";
+	}
+
+	@PutMapping
+	@Transactional
+	public String atualizar(DadosAtualizacaoMarca dados) {
+		var marca = marcaRepository.getReferenceById(dados.id());
+		marca.atualizarInformacoes(dados);
+		return "redirect:marca";
+	}
+
 }
